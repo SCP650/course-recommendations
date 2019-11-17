ea1364cf0b23cfd11916ff46a47091a52cc4ee9b
Sandhya Bala <31396707+sandhyabala31@users.noreply.github.com>
2019-11-16 22:43:59 -0500

    gets top k descriptions + lemmatizes

---
+++ keywordRanksLemma.py
@@ -0,0 +1,49 @@
+#Say we have a list/set of 10 - 15 course description and a dictionary of the 10 keywords and their ranking. 
+#Which 3-5 course should we recommend?
+
+import re
+from nltk.stem import WordNetLemmatizer
+
+#Assumption: lowest ranking is the best
+def rank(courseDescrs, keywordRanking, k):
+
+    ranked = []
+    lemmatizer = WordNetLemmatizer() 
+
+
+    for course in courseDescrs:
+
+        score = 0
+
+        #replace all punctuation stuff with spaces
+        replace = re.sub(r"[,.;@#-?!&$]+", ' ', course)
+        
+        splitCourse = replace.split(" ")
+        splitCourse = list(set(splitCourse))
+        print('\nsplitCourse')
+        print(splitCourse)
+        splitCourse = [ lemmatizer.lemmatize(s) for s in splitCourse]
+
+        for word in splitCourse:
+            if(word in keywordRanking):
+                score += keywordRanking[word]
+
+        print(score)
+        if(score > 0):
+            ranked += [(score, course)]
+
+    print(ranked)
+    ranked.sort(key=lambda x: x[0])
+    print(ranked)
+
+    selected = ranked[:k]
+    print("selected")
+    print(selected)
+    return selected
+
+
+
+d = dict()
+d["yes"] = 3
+d["Hey"] = 6
+rank(["Hey there! yes,", "Hey", "yes-yes"], d, 2)
